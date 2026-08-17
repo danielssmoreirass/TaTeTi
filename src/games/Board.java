0e0b819246839f17ef96a6204cf074986fc5dd1f
Dylan PC <moreiradanix@gmail.com>
2026-08-17 00:55:16 -0300

    New Funtion

--- src/games/Board.java
+++ src/games/Board.java
@@ -4,19 +4,25 @@ package games;
 public class Board {
 
    public Board(){
+    boolean estadoDelJuego = false;
+    char jugador1 = 'X';
+    char jugador2 = 'O';
 
-    int[] number = new int [3];
-    number[0] = 1;
-    number[1] = 4;
-    number[2] = 9;
+    //line1[0] = "x"; //Modo de modificar el array
+    
+    while (estadoDelJuego == false){
+      mostrarTablero();
+      System.out.println("Jugador 1 empieza la partida");
+      break;
+    }
+   }
+   public void mostrarTablero(){
 
     String[] line1 = {"-","-","-"}; 
 
     String[] line2 = {"-","-","-"};
 
     String[] line3 = {"-","-","-"};
-
-    //line1[0] = "x"; //Modo de modificar el array
 
     System.out.print(line1[0] + " | ");
     System.out.print(line1[1] + " | ");
