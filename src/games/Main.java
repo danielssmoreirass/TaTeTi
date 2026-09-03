7f936b1e438cc45d231133934ddf449c02897fe6
Dylan PC <moreiradanix@gmail.com>
2026-09-03 14:19:01 -0300

    Corregi un par de indentaciones

--- src/games/Main.java
+++ src/games/Main.java
@@ -3,7 +3,7 @@ package games;
 public class Main {
  public static void main (String[]args){
 
-   System.out.println("+---------------------------------------+");
+  System.out.println("+---------------------------------------+");
   System.out.println("|         WELCOME TO TA-TE-TI           |");
   System.out.println("+---------------------------------------+");
     
